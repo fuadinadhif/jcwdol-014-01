96a8a69ef96e128940efe5e79faa65f26ffb3617
fuadinadhif <nadhiffuadi@gmail.com>
2024-05-28 17:07:27 +0700

    renew module 01-08

---
+++ 01-programming-fundamental/08-data-structures/exercise/task.js
@@ -0,0 +1,31 @@
+/*
+ * 1. Find the unique values from a list of an Array.
+ * Input: ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+ * Output: ['Hare', 'Khrisna', ':-O']
+ */
+function unique(arr) {
+  // Your code here
+}
+
+/*
+ * 2. Write a function `aclean(arr)` that returns an array cleaned from anagrams.
+ * Example of anagrams:
+ *    A. nap - pan
+ *    B. ear - are - era
+ *    C. cheaters - hectares - teachers
+ * Input: ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"]
+ * Output: ["PAN", hectares, era]
+ */
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+function aclean(arr) {
+  // Your code here
+}
+
+/*
+ * 3. This code has a bug. Solve it!
+ */
+let map = new Map();
+map.set("name", "John");
+let keys = map.keys();
+keys.push("more");
